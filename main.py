4e0ff4ba8b56377f8f512b5fef06a5a95a1da553
Emmet Allen <emmet.allen@gmail.com>
2026-04-20 11:28:22 -0400

    First round of containerization

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import uvicorn
+from arango_cypher.service import app
+
+if __name__ == "__main__":
+    # ServiceMaker/Container Manager usually provides a PORT env var
+    port = int(os.getenv("PORT", "8000"))
+    host = os.getenv("HOST", "0.0.0.0")
+    
+    uvicorn.run(
+        "arango_cypher.service:app",
+        host=host,
+        port=port,
+        reload=False,
+        proxy_headers=True,
+        forwarded_allow_ips="*",
+    )
